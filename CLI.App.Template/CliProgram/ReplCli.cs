063b1ad3c4a93b06e6188af4acc2da26775b911e
atari-monk <atari.monk1@gmail.com>
2023-02-18 19:56:51 +0100

    add foto cli

--- CLI.App.Template/CliProgram/ReplCli.cs
+++ CLI.App.Template/CliProgram/ReplCli.cs
@@ -1,5 +1,4 @@
 using CommandDotNet;
-using CommandDotNet.Repl;
 using CommandDotNet.Unity.Helper;
 using Config.Wrapper;
 using Serilog;
@@ -9,8 +8,6 @@ namespace Modern.CLI.App.Template;
 public class ReplCli 
     : AppProgUnity<ReplCli>
 {
-	private static bool inSession;
-
     [Subcommand]
     public AppCommands? AppCommands { get; set; }
 
@@ -19,23 +16,5 @@ public class ReplCli
         , IConfigReader config) 
             : base(log, config)
     {
-    }
-
-    [DefaultCommand()]
-    public void StartSession(
-        CommandContext context,
-        ReplSession replSession)
-    {
-        if (inSession == false)
-        {
-            context.Console.WriteLine("start session");
-            inSession = true;
-            replSession.Start();
-        }
-        else
-        {
-            context.Console.WriteLine($"no session {inSession}");
-            context.ShowHelpOnExit = true;
-        }
     }
 }
---
+++ Foto.Sort/Program.cs
@@ -0,0 +1,19 @@
+﻿void WriteMenu() {
+  Console.WriteLine("Right arrow - next foto");
+  Console.WriteLine("Enter - delete foto");
+  Console.WriteLine("Space - sort foto");
+  Console.WriteLine("Escape - close app");
+  Console.WriteLine();
+}
+
+ConsoleKeyInfo cki = new ConsoleKeyInfo();
+Console.TreatControlCAsInput = true;
+
+do
+{
+  Console.Clear();
+  WriteMenu();
+  Console.WriteLine(cki.Key.ToString());
+  cki = Console.ReadKey();
+}
+while (cki.Key != ConsoleKey.Escape);
